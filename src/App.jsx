14cb1380310ab988fd329b3f8eb1cf2b6e6ac863
Joseph Broderick <joseph.broderick@endurance.com>
2020-05-11 12:42:18 -0600

    Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
-// import ComponentA from "./components/ComponentA";
-// import ComponentB from "./components/ComponentB";
 import ComponentC from "./components/ComponentC";
 
 export const EmployeeContext = React.createContext();
 export const TeamContext = React.createContext();
 
+const initialState = 2;
+const reducer = (state, action) => {
+  switch(action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      if( state > 0)
+        return state - 1;
+      return 0;
+    case 'reset':
+      return initialState;
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [count, dispatch] = useReducer( reducer, initialState );
+
   return (
     <div className="App">
       <h1>The Office</h1>
-      {/*<ComponentA />*/}
-      {/*<ComponentB />*/}
+      <p>Work for Joe to do: {count} tasks.</p>
+      <button onClick={() => dispatch('decrement')}>Complete 1 Task</button>
+      <button onClick={() => dispatch('increment')}>Scope creep creates 1 new Task</button>
+      <button onClick={() => dispatch('reset')}>Delegate tasks to Nate and Prachi</button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <TeamContext.Provider value={'Trogdor'}>
         <EmployeeContext.Provider value={'Joe Broderick'}>
           <ComponentC />
