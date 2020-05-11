7775b0e1598a388635d5abb0d50ed977e87fbd0e
Joseph Broderick <joseph.broderick@endurance.com>
2020-05-11 13:12:29 -0600

    Working

--- src/App.jsx
+++ src/App.jsx
@@ -6,46 +6,39 @@ export const EmployeeContext = React.createContext();
 export const TeamContext = React.createContext();
 
 const initialState = {
-  tasksForJoe: 2,
-  tasksForTyler: 3,
+  tasks: 2,
 };
+
 const reducer = (state, action) => {
   switch(action.type) {
-    case 'incrementJoe':
-      return {...state, tasksForJoe: state.tasksForJoe + 1};
-    case 'decrementJoe':
-      if( state.tasksForJoe > 0)
-        return {...state, tasksForJoe: state.tasksForJoe - 1};
-      return {...state, tasksForJoe: 0};
-    case 'resetJoe':
-      return {...state, tasksForJoe: initialState.tasksForJoe};
-    case 'incrementTyler':
-      return {...state, tasksForTyler: state.tasksForTyler + 1};
-    case 'decrementTyler':
-      if( state.tasksForTyler > 0)
-        return {...state, tasksForTyler: state.tasksForTyler - 1};
-      return {...state, tasksForTyler: 0};
-    case 'resetTyler':
-      return {...state, tasksForTyler: initialState.tasksForTyler};
+    case 'increment':
+      return {...state, tasks: state.tasks + 1};
+    case 'decrement':
+      if( state.tasks > 0)
+        return {...state, tasks: state.tasks - 1};
+      return {...state, tasks: 0};
+    case 'reset':
+      return {...state, tasks: initialState.tasks};
     default:
       return state;
   }
 };
 
 function App() {
-  const [state, dispatch] = useReducer( reducer, initialState );
+  const [stateJoe, dispatchJoe] = useReducer( reducer, initialState );
+  const [stateTyler, dispatchTyler] = useReducer( reducer, initialState );
 
   return (
     <div className="App">
       <h1>The Office</h1>
-      <p>Work for Joe to do: {state.tasksForJoe} tasks.</p>
-      <button onClick={() => dispatch({type: 'decrementJoe'})}>Complete 1 Task</button>
-      <button onClick={() => dispatch({type: 'incrementJoe'})}>Scope creep creates 1 new Task</button>
-      <button onClick={() => dispatch({type: 'resetJoe'})}>Delegate tasks to Nate and Prachi</button>
-      <p>Work for Tyler to do: {state.tasksForTyler} tasks.</p>
-      <button onClick={() => dispatch({type: 'decrementTyler'})}>Complete 1 Task</button>
-      <button onClick={() => dispatch({type: 'incrementTyler'})}>Scope creep creates 1 new Task</button>
-      <button onClick={() => dispatch({type: 'resetTyler'})}>Delegate tasks to Nate and Prachi</button>
+      <p>Work for Joe to do: {stateJoe.tasks} tasks.</p>
+      <button onClick={() => dispatchJoe({type: 'decrement'})}>Complete 1 Task</button>
+      <button onClick={() => dispatchJoe({type: 'increment'})}>Scope creep creates 1 new Task</button>
+      <button onClick={() => dispatchJoe({type: 'reset'})}>Delegate tasks to Nate and Prachi</button>
+      <p>Work for Tyler to do: {stateTyler.tasks} tasks.</p>
+      <button onClick={() => dispatchTyler({type: 'decrement'})}>Complete 1 Task</button>
+      <button onClick={() => dispatchTyler({type: 'increment'})}>Scope creep creates 1 new Task</button>
+      <button onClick={() => dispatchTyler({type: 'reset'})}>Delegate tasks to Nate and Prachi</button>
       <br />
       <br />
       <br />
