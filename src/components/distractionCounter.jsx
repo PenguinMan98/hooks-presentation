44d19038a8db3afb53274cd86533b70d9da96a2c
Joseph Broderick <joseph.broderick@endurance.com>
2020-05-11 00:29:28 -0600

    Working

--- src/components/distractionCounter.jsx
+++ src/components/distractionCounter.jsx
@@ -3,12 +3,22 @@ import React, { useState } from 'react';
 const DistractionCounter = () => {
   const [count, setCount] = useState(0);
 
+  const addFive = () => {
+    for(let i = 0; i < 5; i++) {
+      setCount( count + 1 );
+    }
+  };
+
+  // const addFive = () => {
+  //   for(let i = 0; i < 5; i++) {
+  //     setCount(prev => prev + 1);
+  //   }
+  // };
+  //
   return (
     <div>
       Number of Distractions Today:
-      <button onClick={() => {
-        setCount(count + 1);
-      }}>
+      <button onClick={addFive}>
         {count}
       </button>
     </div>
