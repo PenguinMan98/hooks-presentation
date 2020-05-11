e2374020797e9dde63e2384d873c89f8d5a2241a
Joseph Broderick <joseph.broderick@endurance.com>
2020-05-11 11:21:21 -0600

    Working

--- src/components/distractionCounter.jsx
+++ src/components/distractionCounter.jsx
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from 'react';
 const DistractionCounter = () => {
   const [name, setName] = useState({first: '', last: ''});
   const [timer, setTimer] = useState(0);
+  const [fullName, setFullName] = useState('');
   let startTime = 0;
+  let tickInterval;
 
   const tick = () => {
     console.log('ticking', startTime, timer);
@@ -11,11 +13,17 @@ const DistractionCounter = () => {
     setTimer(elapsed);
   };
 
+  const startTimer = () => {
+    setFullName(`${name.first} ${name.last}`);
+
+    if(timer === 0){
+      startTime = Date.now();
+
+      tickInterval = setInterval(tick, 1000);
+    }
+  };
+
   useEffect(() => {
-    startTime = Date.now();
-
-    const tickInterval = setInterval(tick, 1000);
-
     return () => {
       clearInterval(tickInterval);
     }
@@ -26,7 +34,8 @@ const DistractionCounter = () => {
       Who is distracting me now?<br />
       <input type="text" onChange={e => setName({...name, first: e.target.value})} value={name.first} /><br />
       <input type="text" onChange={e => setName({...name, last: e.target.value})} value={name.last} /><br />
-      {name.first} {name.last} is!<br />
+      {fullName} is!<br />
+      <button onClick={startTimer}>  Go  </button><br />
       They have been distracting me for {timer} seconds!
     </div>
   );
